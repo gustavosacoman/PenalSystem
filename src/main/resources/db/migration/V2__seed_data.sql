d94ba3cc6451e3e4295267b8d4f4e9d288759275
Ana Paula Endler <anapaulaendler@gmail.com>
2026-04-28 19:23:33 -0300

    fix: seed data id

--- src/main/resources/db/migration/V2__seed_data.sql
+++ src/main/resources/db/migration/V2__seed_data.sql
@@ -119,13 +119,13 @@ INSERT INTO Studies (
     Id, Subject, PrisonerId, Date
 ) VALUES
 (
-    's1s1s1s1-s1s1-s1s1-s1s1-s1s1s1s1s1s1',
+    'f1f1f1f1-f1f1-f1f1-f1f1-f1f1f1f1f1f1',
     'Matemática',
     '11111111-1111-1111-1111-111111111111',
     NOW()
 ),
 (
-    's2s2s2s2-s2s2-s2s2-s2s2-s2s2s2s2s2s2',
+    'f2f2f2f2-f2f2-f2f2-f2f2-f2f2f2f2f2f2',
     'História',
     '33333333-3333-3333-3333-333333333333',
     NOW()
